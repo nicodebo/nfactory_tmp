1b96ed85fac7b2e28df0abd3f3a21a71d3063c0d
unknown <vincentdesaussure@gmail.com>
2018-04-03 16:04:53 +0200

    index peut afficher les afficher

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
+<?php
+include 'include/pdo.php';
+include 'include/model.php';
+
+$movies  = getRandomFilm(10);
 include 'include/header.php';
-include 'include/footer.php';
+?>
+<?php foreach($movies as $key => $movies)?>
+<div class="card">
+    <img src="<?php echo $movie['id'].'jpg'?>">
+</div>
+
+<?php include 'include/footer.php';
