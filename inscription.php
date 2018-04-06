c6c821776113f69d853c176c03855f6b5fe2222f
unknown <vincentdesaussure@gmail.com>
2018-04-06 09:19:58 +0200

    Fix inscription.php

--- inscription.php
+++ inscription.php
@@ -21,60 +21,39 @@ if(!empty($_POST['submitted'])){
    
     //pseudo
     $error = control($error, $pseudo, 'pseudo', 55, 5);
-    // Requete pour checkAlreadyExist() Nico
-    if(!issets($error['pseudo'])){
-        // ICI IL N'Y A PAS D'ERREUR
-        $sql = "SELECT * FROM users WHERE pseudo = :pseudo";
-        $querry = $pdo -> prepare($sql);
-        $querry -> bindValue(':pseudo', $pseudo, PDO::PARAM_STR); // protection
-        $querry -> execute(); // execution
-        $pseudoExist = $querry->fetch(); // je récupere mon utilisateur
-        if(!empty($pseudoExist)) {
-             $error['pseudo'] = 'Pseudo éxiste déjà';         
-        }
+    $pseudoExist = readTableByColVal('pseudo', $pseudo, 'users');
+    if(!empty($pseudoExist)) {
+         $error['pseudo'] = 'Pseudo existe déjà';         
     }
 
     // email
-    $error = controlEmail($email, $error, 'email');
-    // Requete pour checkAlreadyExist() Nico       
-    if(!empty($email)){
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $error['email'] = "Email non valide";
-        } else{ 
-            $sql = "SELECT * FROM users WHERE email = :email";
-            $querry = $pdo -> prepare($sql);
-            $querry -> bindValue(':email', $email, PDO::PARAM_STR);
-            $querry -> execute();
-            $emailExist = $querry->fetch();
-            if(!empty($emailExist)) {
-            $error['email'] = 'Email éxiste déjà'; 
-            }   
-        }
-    }else{
-        $error['email'] ='Veuillez renseigner le champs email';
+    $error = controlEmail($email, $error, 'email', 'users');
+    $emailExist = readTableByColVal('email', $email, 'users');
+    if(!empty($emailExist)) {
+         $error['email'] = 'Email existe déjà';       
     }
 
-    if(!empty($password)&& !empty($confirm_password)){
+    if(!empty($password)&& !empty($confirm_password)) {
         if($password != $confirm_password) { 
             $error['password'] = 'Les mots de passent doivent être identiques';
     
         } elseif (strlen('password') <= 6){
                 $error['password'] = 'Veuillez renseigner au minimum 6 caractères pour ce champs';
-            }
-        }else{
+        }
+    }else{
             $error['password'] ='Veuillez renseigner un mot de passe';
-        } 
+    }
     
     //Requete d'insertion dans le formulaire si celui-ci ne contient pas d'érreur
     if(count($error) == 0){
         $hassPassword = password_hash($password, PASSWORD_DEFAULT);
         $token = generateRandomString(50);
         $role ='user';
-        $sql = "INSERT INTO users (pseudo, email, password, token, created_at, role) VALUES (:pseudo, :email, :password, :token, NOW(), 'abonné')";
+        $sql = "INSERT INTO users (pseudo, email, password, token, created_at, role) VALUES (:pseudo, :email, :password, :token, NOW(), 'abonne')";
         $querry = $pdo -> prepare($sql);
         $querry -> bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
         $querry -> bindValue(':email', $email, PDO::PARAM_STR);
-        $querry -> bindValue(':password', $password, PDO::PARAM_STR);
+        $querry -> bindValue(':password', $hassPassword, PDO::PARAM_STR);
         $querry -> bindValue(':token', $token, PDO::PARAM_STR);
         $querry -> execute();
 
